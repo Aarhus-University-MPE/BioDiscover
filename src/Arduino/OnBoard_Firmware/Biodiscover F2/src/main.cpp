62f654fa4b65d487a6cceec8cd6fbd1afd743563
Fogh-Mec-Ing <146427064+Fogh-Mec-Ing@users.noreply.github.com>
2025-04-07 10:39:57 +0200

    Pass

--- src/Arduino/OnBoard_Firmware/Biodiscover F2/src/main.cpp
+++ src/Arduino/OnBoard_Firmware/Biodiscover F2/src/main.cpp
@@ -23,6 +23,8 @@
 // New camera_30088089
 // Old camera1_23466452
 // Old camera2_23466451
+// Old camera1.2_23734553
+// Old camera2.2_23466442
 #define CAM_ID1           23734553        // ID for camera 1
 #define CAM_ID2           23466442        // ID for camera 2
 #define SYS_SERIAL        "1.5XL-01-03.25"  // System Serial Number (Version-Increment-Prod_Month.Prod_year)
